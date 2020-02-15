d8b7f9b8165e5aaa7acd48116f794f4211380670
fudasatomi <imotas.aduf@gmail.com>
2020-02-15 17:03:59 +0900

    container row col

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,27 @@
 class UsersController < ApplicationController
   def index
+    @user = current_user
+    @users = User.all
+    @book = Book.new
   end
 
   def show
+  	@user = User.find(params[:id])
+
+  	@books = Book.where(user_id:[current_user.id])
+
+  	@book = Book.new
+  end
+
+  def create
+	@book = Book.new(book_params)
+    if @book.save
+    	redirect_to user_path(current_user), flash:{ notice: 'Book was successfully created.'}
+    else
+       @books = Book.where(user_id:[current_user.id])
+
+       render :show
+   end
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
-  resources :users, :except => [:new, :create, :destroy]
+  resources :users, :except => [:new, :destroy]
 
 end
